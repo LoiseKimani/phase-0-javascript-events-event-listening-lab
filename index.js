e531c792459ca51c924ff695a883639e36b4f426
Loise <loise.kimani@student.moringaschool.com>
2022-12-06 14:17:05 +0300

    problematic assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+
+
+const message = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+beforeEach.addingEventListener('click', function() {
+  alert('I was clicked!')
+});
+
+
+
